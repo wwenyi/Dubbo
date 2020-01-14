27d7193bcdae310e8b947f61f1ef58e78cb678fe
1 <1@DESKTOP-A0Q72P1>
2020-01-14 10:33:48 +0800

    Dubbo

--- db/db-common/src/main/java/com/wwy/common/jwtCommon.java
+++ db/db-common/src/main/java/com/wwy/common/jwtCommon.java
@@ -1,20 +1,23 @@
 package com.wwy.common;
 
+
 /**
- * @Author: Helon
- * @Description: JWT使用常量值
- * @Data: Created in 2018/7/27 14:37
- * @Modified By:
+ * JWT使用常量值
+ * @author wwy
+ * @ClassName com.wwy.common.jwtCommon.java
+ * @date 2020年1月14日  上午10:33:06
+ * @version v0.0.1
+ *
  */
 public class jwtCommon {
 
     //签名秘钥
-    public static final String BASE64SECRET = "ZW]4l5JH[m6Lm)LaQEjpb!4E0lRaG(";
+    public static final String BASE64SECRET = "wwy";
 
     //超时毫秒数（默认30分钟）
     public static final int EXPIRESSECOND = 1800000;
 
     //用于JWT加密的密匙
-    public static final String DATAKEY = "u^3y6SPER41jm*fn";
+    public static final String DATAKEY = "wwy";
 
 }
